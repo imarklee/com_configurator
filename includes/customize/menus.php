e0e2fa0099d3e1cda17d3dd960bd321b8cb0cc27
Chris Rault <Chris@chris.local>
2007-09-11 14:10:06 +0200

    random tweaks from the universe.

--- includes/customize/menus.php
+++ includes/customize/menus.php
@@ -10,6 +10,6 @@
 		<p class="teaser">Morph gives you a number of different menus to choose from as well as the ability to customize how they work. We will be adding a full page to explain how to take advantange of the different menus that are available.</p>
 		
 		<p>Most of Morphs menu settings are here, but in order to activate one of the different menu types, you need to add the appropriate module suffix.</p>
-		<p><a href="#" class="topnav-tab btn-link">Configure the topnav block</a>&nbsp;&nbsp;<a href="menufx.html" title="Understanding Morphs menus" class="topnav-tab btn-link modal-link">Learn more about Morph's menus</a></p>
+		<p><a href="#" class="topnav-tab btn-link">Configure the <strong>Top Menu</strong> block</a>&nbsp;&nbsp;<a href="menufx.html" title="Understanding Morphs menus" class="topnav-tab btn-link modal-link">Learn more </a></p>
 	</div>
 </div>

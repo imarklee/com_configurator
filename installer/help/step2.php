049970aa9793676a33885c18eae739a6efc10a63
Chris Rault <Chris@chris.local>
2008-07-22 18:28:46 +0200

    various tweaks

--- installer/help/step2.php
+++ installer/help/step2.php
@@ -1,1 +1,8 @@
-Help Step 2
+<div id="help-two" class="installer-help">
+<h3>Not sure what a themelet is?</h3>
+<p>Themelets as the visual layer of Morph and what people visiting your site will actually see. Built on top of Morphs powerful intelligent engine, themelets inherit all of the underlying awesomeness that is Morph. Not only are themelets quicker to develop, but because they all sit on top of the same framework, which means you always have the same great features close at hand.</p>
+
+<h3>Not sure which file to install?</h3>
+<p>If you haven't downloaded a themelet yet, you will need to login to the club downloads area and <a href="http://www.joomlajunkie.com/member/downloads/" title="login to the club downloads to get the latest versions">grab the latest version</a> of your chosen themelet.</p>
+<p>The file is called <strong>themelet_lightbase_v0.07.zip</strong>, with <em>v0.07</em> referencing the version you have downloaded and will be installing.</p>
+</div>

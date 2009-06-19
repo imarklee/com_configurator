c440b3bf2a99d896a98aa69a47211077a913eb4d
Byron Rode <Byron@byron-rodes-macbook-pro.local>
2009-06-19 12:09:16 +0200

    Added a filesize function that returns a decimal correct filesize on a file size for images in the UI.

--- includes/configurator.functions.php
+++ includes/configurator.functions.php
@@ -1,5 +1,15 @@
 <?php
-
+function formatBytes($bytes, $precision = 2) {
+    $units = array('b', 'kb', 'mb', 'gb', 'tb');
+  
+    $bytes = max($bytes, 0);
+    $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+    $pow = min($pow, count($units) - 1);
+  
+    $bytes /= pow(1024, $pow);
+  
+    return round($bytes, $precision) . $units[$pow];
+} 
 function renderParams($array){
 
 	foreach ($array as $value){

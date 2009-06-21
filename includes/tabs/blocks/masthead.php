3d3cbf7caaf4421a6e4ee8e5260a48a5f6135d46
Chris Rault <Chris@chris.local>
2009-06-21 19:02:43 +0200

    copy update..

--- includes/tabs/blocks/masthead.php
+++ includes/tabs/blocks/masthead.php
@@ -6,7 +6,7 @@
 	</div>
 	<div id="general-info" class="info-panel">
 		<h3>Master head block</h3>
-		<p class="teaser">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+		<p class="teaser">This block is typically used for your sites branding/logo. Check the <a href="">logo tab</a> to customize your logo settings.</p>
 		
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
 	</div>

04b4cd40c274ae9b32e884ffbb690b91c818740d
Chris Rault <Chris@chris.local>
2009-06-21 12:30:33 +0200

    more tweaks.

--- includes/tabs/general/logos.php
+++ includes/tabs/general/logos.php
@@ -5,7 +5,7 @@
 		</ol>
 	</div>
 	<div id="logo-info" class="info-panel">
-		<h3>Logos info</h3>
+		<h3>Customize your sites logo</h3>
 		<p class="teaser">We realize that everyone has different opinions about how your logo should be outputted, so with Morph we have gone ahead and given you option to choose for yourself.</p>
 		
 		<p>The ie6 alternate logo option is a way that you can use a transparent png for all modern browsers that support transparency properly and then use a transparent gif, which isnt as smooth, but works.</p>

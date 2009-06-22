0e0c9764cd6e8ee1d1ed8d8bb19b25e71d1eac5d
Jane Jessica Field <janefield@jane-jessica-fields-macbook-pro.local>
2009-06-22 16:24:04 +0200

    Removed Filestyle on Uploader (it disables the file browser on focus) Fixed overlay.

--- js/configurator.functions.js.php
+++ js/configurator.functions.js.php
@@ -74,12 +74,12 @@ jQuery.noConflict();
 			
 	    /* Inputs and checkboxes --------------
 	    ------------------------------------ */
-	    $("input.input-installer").filestyle({ 
+	    /*$("input.input-installer").filestyle({ 
      		image: "components/com_configurator/images/select-btn.png",
      		imageheight : 27,
      		imagewidth : 81,
      		width : 217
- 		});
+ 		});*/
  		
  		$('.alf-input').focus(function(){
  			if(this.value == 'username' || this.value == 'password'){ 
@@ -269,13 +269,14 @@ jQuery.noConflict();
 		
 		/* Tooltips ----------------------
 		------------------------------- */
+		var arrPageSizes = ___getPageSize();
 		$('<div id="qtip-blanket">').css({
 			position: 'absolute',
 	        top: $(document).scrollTop(),
 	        left: 0,
-	        height: '100%',
-	        width: '100%',
-	  		opacity: 0.7,
+	        width: arrPageSizes[0],
+			height: arrPageSizes[1],
+	        opacity: 0.7,
 	       	backgroundColor: 'black',
 	        zIndex: 5000
 		})
@@ -1012,6 +1013,54 @@ jQuery.noConflict();
 			});
 		return false;
 		});
+		
+		/**
+		/ Third Party Function
+		* getPageSize() by quirksmode.com
+		* @return Array Return an array with page width, height and window width, height
+		*/
+		function ___getPageSize() {
+			var xScroll, yScroll;
+			if (window.innerHeight && window.scrollMaxY) {	
+				xScroll = window.innerWidth + window.scrollMaxX;
+				yScroll = window.innerHeight + window.scrollMaxY;
+			} else if (document.body.scrollHeight > document.body.offsetHeight){ // all but Explorer Mac
+				xScroll = document.body.scrollWidth;
+				yScroll = document.body.scrollHeight;
+			} else { // Explorer Mac...would also work in Explorer 6 Strict, Mozilla and Safari
+				xScroll = document.body.offsetWidth;
+				yScroll = document.body.offsetHeight;
+			}
+			var windowWidth, windowHeight;
+			if (self.innerHeight) {	// all except Explorer
+				if(document.documentElement.clientWidth){
+					windowWidth = document.documentElement.clientWidth; 
+				} else {
+					windowWidth = self.innerWidth;
+				}
+				windowHeight = self.innerHeight;
+			} else if (document.documentElement && document.documentElement.clientHeight) { // Explorer 6 Strict Mode
+				windowWidth = document.documentElement.clientWidth;
+				windowHeight = document.documentElement.clientHeight;
+			} else if (document.body) { // other Explorers
+				windowWidth = document.body.clientWidth;
+				windowHeight = document.body.clientHeight;
+			}	
+			// for small pages with total height less then height of the viewport
+			if(yScroll < windowHeight){
+				pageHeight = windowHeight;
+			} else { 
+				pageHeight = yScroll;
+			}
+			// for small pages with total width less then width of the viewport
+			if(xScroll < windowWidth){	
+				pageWidth = xScroll;		
+			} else {
+				pageWidth = windowWidth;
+			}
+			arrayPageSize = new Array(pageWidth,pageHeight,windowWidth,windowHeight);
+			return arrayPageSize;
+		};
 		
 	});
 })(jQuery);

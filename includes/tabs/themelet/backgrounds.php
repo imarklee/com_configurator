724da7fbc8c238fe1158d9e3bfc31f64424f12a2
Chris Rault <Chris@chris.local>
2009-06-23 15:25:47 +0200

    bg help info

--- includes/tabs/themelet/backgrounds.php
+++ includes/tabs/themelet/backgrounds.php
@@ -8,6 +8,19 @@
 		<h3>Backgrounds overview</h3>
 		<p class="teaser">These options allow you to easily customize your sites background.</p>
 		
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+		<p>Looking for a custom background pattern that you can use for your site? We have compiled a comprehensive list of different resources to help you build your perfect site. 
+		<a href="#">Click here for more info</a>.</p>
+
+
+		<p>Not sure about which colors to choose? Here is a list of our favorite (free) color palette generators:</p>
+		
+		<ul class="morph-resources">
+			<li><a href="http://www.colorotate.org" title="click here to open this site in a new window" target="_blank">ColoRotate</a></li>
+			<li><a href="http://aviary.com/tools/toucan" title="click here to open this site in a new window" target="_blank">Toucan</a></li>
+			<li><a href="http://www.colourlovers.com/copaso" title="click here to open this site in a new window" target="_blank">Copaso</a></li>
+			<li><a href="http://kuler.adobe.com" title="click here to open this site in a new window" target="_blank">Adobe Kuler</a></li>
+			<li><a href="http://colorschemedesigner.com" title="click here to open this site in a new window" target="_blank">Color Scheme Designer</a></li>
+			<li><a href="http://colorexplorer.com" title="click here to open this site in a new window" target="_blank">ColorExplorer</a></li>
+		</ul>
 	</div>
 </div>

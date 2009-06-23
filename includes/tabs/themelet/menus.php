a0ec62d87d16c1ba5019a9b6d2501ae197dfc5ec
Chris Rault <Chris@chris.local>
2009-06-23 17:45:18 +0200

    more tweaking

--- includes/tabs/themelet/menus.php
+++ includes/tabs/themelet/menus.php
@@ -8,6 +8,6 @@
 		<h3>Menu overview</h3>
 		<p class="teaser">Morph gives you a number of different menus to choose from as well as the ability to customize how they work. We will be adding a full page to explain how to take advantange of the different menus that are available.</p>
 		
-		<p>Most of Morphs menu settings are here, but in order to activate one of the different menu types, you need to add the appropriate module suffix. <a href="menus.html" title="Understanding Morphs menus" class="modal-link" target="_blank">Click here</a> for a list of available suffixes, which explainations on each.</p>
+		<p>Most of Morphs menu settings are here, but in order to activate one of the different menu types, you need to add the appropriate module suffix. <a href="menus.html" title="Understanding Morphs menus" class="modal-link" target="_blank">Click here</a> for a list of available suffixes, with explainations on each.</p>
 	</div>
 </div>

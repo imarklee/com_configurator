620c69f05ef5ca66b8515525800efc5bb62477bb
Chris Rault <Chris@chris.local>
2009-06-25 16:05:33 +0200

    updated links

--- includes/footer.php
+++ includes/footer.php
@@ -6,33 +6,33 @@
 			<li class="fa">
 				<h4>JoomlaJunkie</h4>
 				<ul>
-					<li><a href="#start">Team Blog</a></li>
-					<li><a href="#backgrounds">Template Demos</a></li>
-					<li><a href="#colors">Members Access</a></li>
+					<li><a href="http://www.prothemer.com/blog" target="_blank">Team Blog</a></li>
+					<li><a href="http://www.joomlajunkie.com/demo" target="_blank">Template Demos</a></li>
+					<li><a href="http://www.joomlajunkie.com/secure" target="_blank">Members Access</a></li>
 				</ul>
 			</li>
 			<li class="fb">
 				<h4>Help &amp; Support</h4>
 				<ul>
-					<li><a href="#start">Knowledgebase</a></li>
-					<li><a href="#backgrounds">Documentation</a></li>
-					<li><a href="#colors">Support Forum</a></li>
+					<li><a href="http://www.joomlajunkie.com/contact/index.php?act=kb" target="_blank">Knowledgebase</a></li>
+					<li><a href="http://www.joomlajunkie.com/morph2" target="_blank">Documentation</a></li>
+					<li><a href="http://www.joomlajunkie.com/member" target="_blank">Support Forum</a></li>
 				</ul>
 			</li>
 			<li class="fc">
 				<h4>Morph &amp; Configurator</h4>
 				<ul>
-					<li><a href="#start">Get to know Morph</a></li>
-					<li><a href="#backgrounds">Beta Forum</a></li>
-					<li><a href="#colors">Themelets Voting</a></li>
+					<li><a href="http://www.joomlajunkie.com/morph" target="_blank">Get to know Morph</a></li>
+					<li><a href="http://www.joomlajunkie.com/member/beta" target="_blank">Beta Forum</a></li>
+					<li><a href="http://www.joomlajunkie.com/themelets" target="_blank">Themelets Voting</a></li>
 				</ul>
 			</li>
 			<li class="fd">
 				<h4>Follow us on Twitter</h4>
 				<ul>
-					<li><a href="#start"><strong>@jMorph</strong> - For Morph only updates</a></li>
-					<li><a href="#backgrounds"><strong>@joomlajunkies</strong> - JoomlaJunkie news</a></li>
-					<li><a href="#colors"><strong>@joomlatips</strong> - Useful Joomla tips</a></li>
+					<li><a href="http://twitter.com/jMorph" target="_blank"><strong>@jMorph</strong> - For Morph only updates</a></li>
+					<li><a href="http://twitter.com/joomlajunkies" target="_blank"><strong>@joomlajunkies</strong> - JoomlaJunkie news</a></li>
+					<li><a href="http://twitter.com/joomlatips" target="_blank"><strong>@joomlatips</strong> - Useful Joomla tips</a></li>
 				</ul>
 			</li>
 		</ul>

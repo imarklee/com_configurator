2179d968a0960fe9f7b9d8d0a627ed55afdd8c5f
Chris Rault <Chris@chris.local>
2009-06-30 16:35:59 +0200

    tweak

--- includes/help.php
+++ includes/help.php
@@ -1,16 +1,16 @@
 <div class="help-block1">
 <h3>Inline Help &amp; Documentation:</h3>
-<p>We have jam packed extensive documentation into Morph, explaining each and every option that is available.</p>
-<p class="help-links"><a href="#" target="_blank" class="btn-link">View the help index</a></p>
+<p>We are working on extensive documentation for Morph &amp; Configurator, explaining each and every option that is available.</p>
+<p class="help-links"><a href="#" target="_blank" class="btn-link disabled" title="coming soon!">View the help index</a></p>
 <h3>Beta Testers Support Board:</h3>
-<p>We have added a special beta testers board to our forum, where you can submit any bugs you find, provide us with feedback or share an idea you may have on how we can make Morph even better.</p>
+<p>You can submit bugs, provide us with feedback or share ideas you have on how we can make Morph even better.</p>
 <p class="help-links"><a href="http://www.joomlajunkie.com/members/morph" target="_blank" class="btn-link">Login to the forums</a></p>
 </div>
 
 <div class="help-block2">
 <h3>Morph Documentation Site:</h3>
-<p>The online documentation site will grow extensively over the next couple of months, with everything from documenting every aspect of Morph, extensive tutorials, screencasts &amp; more.</p>
+<p>The online documentation site will grow extensively and cover every aspect of Morph with tutorials, screencasts &amp; more.</p>
 <p class="help-links"><a href="http://www.joomlajunkie.com/morph2" target="_blank" class="btn-link">Visit the documentation site</a></p>
 <h3>Twitter Updates:</h3>
-<p>We want to be incredibly easy for you to stay informed about Morphs progress, so we have created a twitter account just for MorphUpdates. Follow this account to get up to the minute updates.</p>
+<p>You can easily stay informed about Morphs progress - follow jMorph on Twitter and get up to the minute updates!</p>
 <p class="help-links"><a href="http://twitter.com/jMorph" target="_blank" class="btn-link">Follow @jMorph on Twitter</a></p></div>
--- includes/tabs/desc/desc-tools.php
+++ includes/tabs/desc/desc-tools.php
@@ -2,7 +2,7 @@
 <div id="tools-desc" class="tab-desc" <?php if (isset ($_COOKIE['tools-desc'])) { ?> style="display:none;"<?php } ?>>
 	<div class="desc-inner">
 		<h2>About the tools tab</h2>
-		<p class="last">At the moment there is only the installer tool, but we plan on adding a bunch of other useful tools here once Configurator &amp; Morph reach a stable release.
+		<p class="last">At the moment there is only the installer tool, but we plan on adding a bunch of other useful tools here once Configurator &amp; Morph reach a stable release. <br /><br />We know the uploader tool isnt exactly pretty yet, but we promise it will be super awesome before the stable release ;)
 		<a href="#">close</a></p>
 	</div>
 </div>

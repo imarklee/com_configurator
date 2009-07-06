0215a5e46c89077881a1152b33fc12db28c80090
Chris Rault <Chris@chris.local>
2009-07-06 18:36:13 +0200

    tweak

--- includes/help.php
+++ includes/help.php
@@ -1,7 +1,7 @@
 <div class="help-block1">
 <h3>Inline Help &amp; Documentation:</h3>
 <p>We are working on extensive documentation for Morph &amp; Configurator, explaining each and every option that is available.</p>
-<p class="help-links"><a href="#" target="_blank" class="btn-link disabled" title="coming soon!">View the help index</a></p>
+<!--<p class="help-links"><a href="#" target="_blank" class="btn-link disabled" title="coming soon!" title="coming soon!">View the help index</a></p>-->
 <h3>Beta Testers Support Board:</h3>
 <p>You can submit bugs, provide us with feedback or share ideas you have on how we can make Morph even better.</p>
 <p class="help-links"><a href="http://www.joomlajunkie.com/members/morph" target="_blank" class="btn-link">Login to the forums</a></p>
@@ -10,7 +10,7 @@
 <div class="help-block2">
 <h3>Morph Documentation Site:</h3>
 <p>The online documentation site will grow extensively and cover every aspect of Morph with tutorials, screencasts &amp; more.</p>
-<p class="help-links"><a href="http://www.joomlajunkie.com/morph2" target="_blank" class="btn-link">Visit the documentation site</a></p>
+<!--<p class="help-links"><a href="http://www.joomlajunkie.com/morph2" target="_blank" class="btn-link disabled" title="coming soon!">Visit the documentation site</a></p>-->
 <h3>Twitter Updates:</h3>
 <p>You can easily stay informed about Morphs progress - follow jMorph on Twitter and get up to the minute updates!</p>
 <p class="help-links"><a href="http://twitter.com/jMorph" target="_blank" class="btn-link">Follow @jMorph on Twitter</a></p></div>

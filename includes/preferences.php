6b022e0a203ef1b2f2b84f3ee5011a8347886be9
Byron Rode <Byron@vc-41-3-182-108.umts.vodacom.co.za>
2009-07-07 15:48:33 +0200

    preferences screen

--- includes/preferences.php
+++ includes/preferences.php
@@ -1,3 +1,37 @@
-<h2>Coming Soon</h2>
-<p>the preferences screen is coming soon</p>
-<a href="#" class="close-preferences">close</a>
+<h2>Preferences</h2>
+<div id="info">
+Set your configurator preferences...
+</div>
+<form action="#" method="post" id="preferences-form">
+<ul class="prefs">
+	<li>
+	<label>Show Quick Tips: 
+		<span><input type="radio" name="showtips" value="1" />Yes</span>
+		<span><input type="radio" name="showtips" value="0" />No</span>
+	</label>
+	</li>
+	<li>
+	<label>Show Tab Introductions: 
+		<span><input type="radio" name="showintros" value="1" />Yes</span>
+		<span><input type="radio" name="showintros" value="0" />No</span>
+	</label>
+	</li>
+	<li>
+	<label>Sortable Tabs: 
+		<span><input type="radio" name="showtips" value="1" />Yes</span>
+		<span><input type="radio" name="showtips" value="0" />No</span>
+	</label>
+	</li>
+	<li>
+	<label>Automagically check for updates:
+		<span><input type="radio" name="showtips" value="1" />Yes</span>
+		<span><input type="radio" name="showtips" value="0" />No</span>
+	</label>
+	</li>
+</ul>
+<input type="submit" value="save preferences" />
+</form>
+============================= split here ===============================
+<div id="system">
+	<a href="#" class="check-updates">Check for updates</a>
+</div>

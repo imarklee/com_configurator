3a74f2e7a753762fdd9de9ab0ba444f8b3b7a1ce
Byron Rode <Byron@Byrons-Mactizzy.local>
2009-07-10 17:04:27 +0200

    Installer base files created

---
+++ includes/installer/step2.php
@@ -0,0 +1,28 @@
+<div id="sd-header">
+		<!-- <img src="components/com_configurator/images/morph-logo.png" alt="morph logo" width="173" height="60" border="0" /> -->
+		<img src="../com_configurator/images/morph-logo.png" alt="morph logo" width="173" height="60" border="0" />
+		<p class="steps"><strong>Step 2 of 4: </strong>Install Template Framework</p>
+	</div>
+	<div id="sd-body">
+		<div id="sd-text">
+			
+			<h3>Install the Morph template framework</h3>
+			<p>We are now going to install the Morph Framework. If you have a previously installed version,
+			you can either choose to skip this step or proceed with the install. If you proceed, a backup
+			of the previous version will automatically be created.</p>
+			<form id="install-template" method="post" action="#">
+				<label>
+					<h4>Select the template framework install file:</h4>
+					<input type="file" name="template-file" id="template-file" />
+				</label>
+			
+		</div>
+		<input class="action" type="submit" value="submit" />
+		<!--<a href="#" class="btn-install">Install Template</a> old button -->
+		</form>
+		<a href="index.php?option=com_configurator&task=manage" class="btn-skip">Skip this step</a>
+	</div>
+	<div id="sd-image">
+		<!-- <img src="components/com_configurator/images/loader3.gif" height="16" width="16" border="0" align="center" alt="Loading" /> -->
+		<img src="../com_configurator/images/loader3.gif" height="16" width="16" border="0" align="center" alt="Loading" />
+	</div>

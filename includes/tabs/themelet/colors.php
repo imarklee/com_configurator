7ac1269227c2e303944441f98e1863553d5cac28
Chris Rault <Chris@chris.local>
2009-07-23 18:09:04 +0200

    general tweaks + dialogs

--- includes/tabs/themelet/colors.php
+++ includes/tabs/themelet/colors.php
@@ -7,10 +7,10 @@
 	<div id="color-info" class="info-panel">
 		<h3>Customizing your colors</h3>
 		<p class="teaser">We realize that not every themelet design is ever going to be a 100% perfect fit for your business, so we have given you a couple of options to customize the colors to fit you own brand.</p>
-		
+	
 		<p>Not sure about which colors to choose? Here is a list of our favorite (free) color palette generators:</p>
 		
-		<ul class="morph-resources">
+		<ul class="resources-list">
 			<li><a href="http://www.colorotate.org" title="click here to open this site in a new window" target="_blank">ColoRotate</a></li>
 			<li><a href="http://www.colourlovers.com/copaso" title="click here to open this site in a new window" target="_blank">Copaso</a></li>
 			<li><a href="http://kuler.adobe.com" title="click here to open this site in a new window" target="_blank">Adobe Kuler</a></li>

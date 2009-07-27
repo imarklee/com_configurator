187c51a9947365987bb3644e7383d8eab2e95af4
Chris Rault <Chris@chris.local>
2009-07-27 18:56:53 +0200

    footer links

--- includes/footer.php
+++ includes/footer.php
@@ -4,34 +4,34 @@
 		<a href="http://www.prothemer.com" target="_blank" class="ptlogo">ProThemer</a>
 		<ul class="footer-links">
 			<li class="fa">
-				<h4>JoomlaJunkie</h4>
+				<h4>Stay updated</h4>
 				<ul>
-					<li><a href="http://www.prothemer.com/blog" target="_blank">Team Blog</a></li>
-					<li><a href="http://www.joomlajunkie.com/demo" target="_blank">Template Demos</a></li>
-					<li><a href="http://www.joomlajunkie.com/secure" target="_blank">Members Access</a></li>
+					<li><a href="http://www.prothemer.com/blog" target="_blank">Prothemer blog</a></li>
+					<li><a href="http://www.joomlajunkie.com/demo" target="_blank">Subscribe to newsletter</a></li>
+					<li><a href="http://www.joomlajunkie.com/secure" target="_blank">Login to the club</a></li>
 				</ul>
 			</li>
 			<li class="fb">
 				<h4>Help &amp; Support</h4>
 				<ul>
-					<li><a href="http://www.joomlajunkie.com/contact/index.php?act=kb" target="_blank">Knowledgebase</a></li>
-					<li><a href="#" target="_blank" title="coming soon!" onclick="return false">Documentation</a></li>
-					<li><a href="http://www.joomlajunkie.com/member/#morph" target="_blank">Support Forum</a></li>
+					<li><a href="http://www.joomlajunkie.com/contact/index.php?act=kb&code=cat&id=7" target="_blank">Frequently asked questions</a></li>
+					<li><a href="http://www.joomlajunkie.com/member/#morph" target="_blank">Morph beta support forum</a></li>
+					<li><a href="http://www.joomlajunkie.com/member/downloads/morph-beta-21/" target="_blank">Download latest versions</a></li>
 				</ul>
 			</li>
 			<li class="fc">
 				<h4>Morph &amp; Configurator</h4>
 				<ul>
 					<li><a href="http://www.prothemer.com/morph" target="_blank">Learn more about Morph</a></li>
-					<li><a href="http://www.prothemer.com/demo/joomla/morph/blocks" target="_blank">Interactive demo</a></li>
-					<li><a href="http://www.joomlajunkie.com/themelets" target="_blank">Vote for themelets</a></li>
+					<li><a href="http://www.prothemer.com/demo/joomla/morph/blocks" target="_blank">Try out the interactive demo</a></li>
+					<li><a href="http://www.joomlajunkie.com/themelets" target="_blank">Vote for your favorite themelets</a></li>
 				</ul>
 			</li>
 			<li class="fd">
 				<h4>Follow us on Twitter</h4>
 				<ul>
 					<li><a href="http://twitter.com/jMorph" target="_blank"><strong>@jMorph</strong> - For Morph only updates</a></li>
-					<li><a href="http://twitter.com/joomlajunkies" target="_blank"><strong>@prothemer</strong> - Prothemer news</a></li>
+					<li><a href="http://twitter.com/prothemer" target="_blank"><strong>@prothemer</strong> - Prothemer news</a></li>
 					<li><a href="http://twitter.com/joomlatips" target="_blank"><strong>@joomlatips</strong> - Useful Joomla tips</a></li>
 				</ul>
 			</li>

3e99b41faa88ce1802a412554630702c294801df
Byron Rode <Byron@Byrons-Mactizzy.local>
2009-08-05 12:25:15 +0200

    Updated installer file

--- install.configurator.php
+++ install.configurator.php
@@ -1,15 +1,41 @@
 <?php 
+ob_start();
 (strpos($_SERVER['SCRIPT_NAME'], 'install.configurator.php') === false) ? $base = './components/com_configurator' : $base = '.';
 ini_set('memory_limit', '32M');
 define('JINDEXURL', $base);
-
 setcookie('installed_cfg', 'true');
-?>
-	
-<script src="<?php echo JINDEXURL; ?>/installer/js/install.js.php" type="text/javascript"></script>
-<link href="<?php echo JINDEXURL; ?>/installer/css/install.css.php" media="screen" rel="stylesheet" type="text/css" />
 
-<script type="text/javascript"></script>
+define('ROOT', str_replace(array('/administrator/components/com_configurator','\administrator\components\com_configurator'), '', dirname(__FILE__)));
+$backupdir = ROOT . DS . 'morph_assets' . DS . 'backups';
+$logosdir = ROOT . DS . 'morph_assets' . DS . 'logos';
+$backgroundsdir = ROOT . DS . 'morph_assets' . DS . 'backgrounds';
+$themeletsdir = ROOT . DS . 'morph_assets' . DS . 'themelets';
+
+// create assets folder
+if(!is_dir(ROOT . DS . 'morph_assets')){
+	if(!@mkdir(ROOT . DS . 'morph_assets')) { $error = true; }else{ JPath::setPermissions(ROOT . DS . 'morph_assets'); }
+
+	if(!is_dir($backupdir)){
+		(!@mkdir($backupdir)) ? $error = true : JPath::setPermissions($backupdir);
+	}
+
+	if(!is_dir($logosdir)){
+		(!@mkdir($logosdir)) ? $error = true : JPath::setPermissions($logosdir);
+	}
+	
+	if(!is_dir($backgroundsdir)){
+		(!@mkdir($backgroundsdir)) ? $error = true : JPath::setPermissions($backgroundsdir);
+	}
+	
+	if(!is_dir($themeletsdir)){
+		(!@mkdir($themeletsdir)) ? $error = true : JPath::setPermissions($themeletsdir);
+	}
+}
+
+$document = JFactory::getDocument();
+$document->addScript(JURI::root() . 'administrator/components/com_configurator/installer/js/install.js.php');
+$document->addStyleSheet(JURI::root() . 'administrator/components/com_configurator/installer/css/install.css.php');
+?>
 <div id="install-wrap">
 	<div id="installer">
 	<?php
@@ -31,3 +57,5 @@ setcookie('installed_cfg', 'true');
 </div>
 <div id="dialog" style="display:none;"></div>
 <div id="help-dialog" style="display:none;"></div>
+<?php if(isset($error) && $error){ include 'installer/error.php'; } ?>
+<?php ob_end_flush(); ?>

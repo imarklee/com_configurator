e76ed63312f9c544cfbc1438a86a3ac7fc9c0008
Chris Rault <Chris@chris.local>
2009-08-27 10:50:12 +0200

    css restructuring

--- css/configurator.css.php
+++ css/configurator.css.php
@@ -1,11 +1,40 @@
 <?php
 ob_start();
 header('content-type: text/css; charset: UTF-8');
-include('reset.css');
-include('960.css');
-include('jquery.ui.css');
-include('manage.css');
-include('colorpicker.css');
-include('text.css');
+
+// global
+include('global/reset.css');
+include('global/960.css');
+include('global/ui.css');
+include('global/text.css');
+include('global/overlay.css');
+include('global/sprite.css');
+
+// dashboard
+include('dashboard/dashboard.css');
+
+// login
+include('login/login.css');
+
+// manage
+include('manage/assets.css');
+include('manage/colorpicker.css');
+include('manage/docs.css');
+include('manage/feedback.css');
+include('manage/footer.css');
+include('manage/forms.css');
+include('manage/fullscreen.css');
+include('manage/help.css');
+include('manage/keyboard.css');
+include('manage/manage.css');
+include('manage/preferences.css');
+include('manage/shelf.css');
+include('manage/tabs.css');
+include('manage/tips.css');
+include('manage/tooltips.css');
+include('manage/toplinks.css');
+include('manage/uploader.css');
+include('manage/welcome.css');
+
 ob_end_flush();
 ?>

e5520610ac5dbe2d121c09489098d34dd1d56007
Chris Rault <Chris@chris.local>
2009-09-04 17:55:00 +0200

    plugins tab description..

--- includes/plugins/desc-plugins.php
+++ includes/plugins/desc-plugins.php
@@ -2,7 +2,7 @@
 <div id="plugins-desc" class="tab-desc"<?php if (isset ($_COOKIE['plugins-desc'])) { ?> style="display:none;"<?php } ?>>
 	<div class="desc-inner">
 		<h2>About the plugins tab</h2>
-		<p class="last">Description will go here.
+		<p class="last">The plugins tab will provide you with an interface to manage the various jQuery plugins that we have integrated with Morph. 
 		<a href="#">close</a></p>
 	</div>
 </div>

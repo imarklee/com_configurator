83b200db8a055ce111f72f1bb4661669bca27c2b
Chris Rault <Chris@chris.local>
2009-09-04 18:16:20 +0200

    captify tab description.

--- includes/plugins/captify.php
+++ includes/plugins/captify.php
@@ -6,7 +6,16 @@
 	</div>
 	<div id="captify-info" class="info-panel">
 		<h3>Fancy captions</h3>
-		<p class="teaser">Captions description will go here.</p>
-		<p>And some more here.</p>
+		<p class="teaser">This plugin lets you easily add fancy captions to any of your content's images.</p>
+		<p>To use, simply add an image to your Joomla article using the default insert image button, then in the image lightbox:</p>
+	    <ol>
+    		<li>Add the text you would like displayed in the <strong>image description</strong> input.</li>
+    		<li>Optionally add a <strong>title</strong> describing the image (recommended).
+    		<li>Tick the <strong>caption</strong> checkbox.</li>
+    		<li>Set the <strong>image alignment</strong>.</li>
+    		<li>Click the <strong>insert</strong> button on the top right of the popup.</li>
+        </ol>
+		<p></p>
 	</div>
 </div>
+

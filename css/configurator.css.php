25ee8f6ed88b157d9537de0bcaa2b3f805861794
Chris Rault <Chris@chris.local>
2009-09-16 20:23:32 +0200

    dimwit

--- css/configurator.css.php
+++ css/configurator.css.php
@@ -6,11 +6,11 @@ if(!ob_start("ob_gzhandler")) ob_start();
 ob_start();
 }
 header("cache-control: must-revalidate");$offset = 60 * 10000;$expire = "expires: " . gmdate("D, d M Y H:i:s", time() + $offset) . " GMT";header($expire);
-
 // global
 include('reset.css');
 include('960.css');
 include('ui.css');
+include('text.css');
 include('overlay.css');
 include('sprite.css');
 include('manage.css');
@@ -21,14 +21,13 @@ include('login.css');
 // manage
 include('welcome.css');
 include('toplinks.css');
-include('accordion.css');
-include('help.css');
 include('assets.css');
 include('tabs.css');
-include('tips.css');
+include('accordion.css');
 include('shelf.css');
 include('forms.css');
 include('footer.css');
+include('help.css');
 include('keyboard.css');
 include('preferences.css');
 include('tooltips.css');
@@ -37,6 +36,7 @@ include('colorpicker.css');
 include('feedback.css');
 include('docs.css');
 include('fullscreen.css');
+include('tips.css');
 }
 ob_end_flush();
 ?>

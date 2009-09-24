df76ad9fa2c36036ed869fe34849c5df17ad0a06
Chris Rault <Chris@chris.local>
2009-09-24 13:24:53 +0200

    tweaks

--- installer/help/step1.php
+++ installer/help/step1.php
@@ -1,8 +1,8 @@
 <div id="help-one" class="installer-help">
-<h3>How the automatic backup works:</h3>
-<p>If you have a copy of Morph installed, you can choose to have a backup automatically created for you. Backups are stored in sites assets folder, which is located in "<em>[site-root]/morph_assets</em>"</p>
+<h3>Installing the Morph template:</h3>
+<p>Now that you have installed Configurator, you can proceed to installing the Morph template, which is where most of the framework files and functionality is kept.</p>
 
 <h3>Not sure which file to install?</h3>
 <p>If you haven't downloaded Morph yet, you will need to login to the club downloads area and <a href="http://www.joomlajunkie.com/member/downloads/" title="login to the club downloads to get the latest versions">grab the latest version</a> of the Morph template.</p>
-<p>The file you are looking for starts with <strong>morph_template_</strong>, with followed by the version and date stamp. An example of this is template_morph_v009_1248716802.zip.</p>
+<p>The file you are looking for starts with <strong>template_morph_</strong> and is followed by the version. An example of this would be template_morph_v0.17.tgz, where the version is 0.17.</p>
 </div>

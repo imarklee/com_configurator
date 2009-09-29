911b420bf05016e782d3e4e298269bb3bb0c8d03
Chris Rault <Chris@chris.local>
2009-09-29 15:55:38 +0200

    last tweaks

--- includes/tools/reset.php
+++ includes/tools/reset.php
@@ -5,7 +5,8 @@ $curr_themelet = $db->loadResult($query);
 ?>
 <div id="reset-settings">
 	<h2>Reset Configurator's Settings</h2>
-	<div class="warning"><div class="warning-inner">You can use this tool to reset your Configurator Settings and/or Preferences. This tool is not recommended on production
+	<div class="warning"><div class="warning-inner"><h3>Important: Please Read!</h3>
+	You can use this tool to reset your Configurator Settings and/or Preferences. This tool is not recommended on production
 		sites as it will revert your install back to its initial setup. <strong>Use with caution.</strong></div></div>
 	<ul>
 	    <li><label><input type="checkbox" name="reset-prefs" value="prefs" /> Reset your Configurator Preferences</label></li>

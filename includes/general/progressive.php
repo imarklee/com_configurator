db4aae21ea4df28dbec51beef957377533ce09d1
Chris Rault <Chris@chris.local>
2009-10-08 13:30:27 +0200

    title tweak

--- includes/general/progressive.php
+++ includes/general/progressive.php
@@ -6,7 +6,7 @@
 		</ol>
 	</div>
 	<div id="progressive-info" class="info-panel">
-		<h3>Progressive overview</h3>
+		<h3>Progressive Enhancements overview</h3>
 		<p class="teaser">These are the subtle enhancements that are layered onto your site, as browsers can support them. Morph has a number of these that take place automatically in the background.</p>
 		
 		<p>Some examples of progressive enhancements that take place in Morph include the adding of first &amp; last classes where needed, custom classes added to form elements, article intro paragraph, image aligment classes, zebra striping on table rows, rounded corners, equal heights &amp; more.</p>

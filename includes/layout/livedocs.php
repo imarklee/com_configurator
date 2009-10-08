4376c23a6a14cac6ce04f53f7c35b5bb3d5f6dcc
Chris Rault <Chris@chris.local>
2009-10-08 11:00:35 +0200

    livedocs frameset.

--- includes/layout/livedocs.php
+++ includes/layout/livedocs.php
@@ -1,1 +1,1 @@
-<iframe id="livedocs" name="livedocs" class="autoHeight" scrolling="auto" frameborder="0" width="100%" height="auto" src="http://www.prothemer.com/joomla/morph/livedocs/"></iframe>
+<iframe id="livedocs" name="livedocs" class="autoHeight" scrolling="auto" frameborder="0" width="100%" height="1500px" src="http://www.prothemer.com/joomla/morph/livedocs/"></iframe>

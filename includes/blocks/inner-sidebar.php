ee96cc4e3390aa433ac2930d761d29485bafb890
Chris Rault <Chris@chris.local>
2009-12-04 14:21:30 +0200

    sidebar module names.

--- includes/blocks/inner-sidebar.php
+++ includes/blocks/inner-sidebar.php
@@ -1,28 +1,32 @@
 <div id="inner-sidebar-tab" class="ui-tabs-hide ui-tabs-panel">
 	<div id="inner-sidebar-options" class="options-panel">
-	    <h3>Inner Sidebar (Right) Settings</h3>
+	    <h3>Inner Sidebar Defaults</h3>
 		<ol class="forms">
 			<?php echo renderParams($params->renderToArray('innersidebar', 'innersidebardata')); ?>
 		</ol>
-	    <h3>Split Right Settings</h3>
+	    <h3>Inner1 Position</h3>
 		<ol class="forms">
-			<?php echo renderParams($params->renderToArray('splitright', 'splitrightdata')); ?>
+			<?php echo renderParams($params->renderToArray('inner1', 'inner1data')); ?>
 		</ol>
-	    <h3>Top Right Settings</h3>
+	    <h3>Inner2 Position</h3>
 		<ol class="forms">
-			<?php echo renderParams($params->renderToArray('topright', 'toprightdata')); ?>
+			<?php echo renderParams($params->renderToArray('inner2', 'inner2data')); ?>
 		</ol>
-	    <h3>Right Settings</h3>
+	    <h3>Inner3 Position</h3>
 		<ol class="forms">
-			<?php echo renderParams($params->renderToArray('right', 'rightdata')); ?>
+			<?php echo renderParams($params->renderToArray('inner3', 'inner3data')); ?>
 		</ol>
-	    <h3>Bottom Right Settings</h3>
+	    <h3>Inner4 Position</h3>
 		<ol class="forms">
-			<?php echo renderParams($params->renderToArray('bottomright', 'bottomrightdata')); ?>
+			<?php echo renderParams($params->renderToArray('inner4', 'inner4data')); ?>
+		</ol>
+	    <h3>Inner5 Position</h3>
+		<ol class="forms">
+			<?php echo renderParams($params->renderToArray('inner5', 'inner5data')); ?>
 		</ol>
 	</div>
 	<div id="inner-sidebar-info" class="info-panel">
-		<h3>Inner sidebar (right)</h3>
+		<h3>Inner Sidebar</h3>
 		<p class="teaser">The concept of the inner &amp; outer layouts require a bit of understanding to 
 		grasp, but once you do you will have unparalelled flexibility at your finger tips.</p>
 		<p>Based on commonly accepted <em>best practices</em>, we refer to your sites main content as your 

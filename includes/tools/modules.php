ebdcee1df562453afabda57a2104146d8d2e05ad
Byron Rode <Byron@vc-41-7-210-120.umts.vodacom.co.za>
2009-12-08 11:57:37 +0200

    Migrator Reset

--- includes/tools/modules.php
+++ includes/tools/modules.php
@@ -60,5 +60,19 @@ for($i=1;$i<=$new_count;$i++){ $new_module_positions[] = 'outer'.$i; }
 	<div id="migrate-submit">
 		<a href="#" class="btn-link">Migrate Modules</a>
 	</div>
+	
+	<div id="migrate-reset">
+		<h2>Reset Module Positions</h2>
+		<ul>
+			<li class="migrate-outer">
+				<a href="#" class="btn-link" action="outer">Reset all Outer Positions to Left Position</a>
+				<p>This will reset all the Outer Positions (Outer 1 - Outer 5) back to the Left position</p>
+			</li>
+			<li class="migrate-inner">
+				<a href="#" class="btn-link" action="inner">Reset all Inner Positions to Right Position</a>
+				<p>This will reset all the Inner Positions (Inner 1 - Inner 5) back to the Right position</p>
+			</li>
+		</ul>
+	</div>
 
 </div>

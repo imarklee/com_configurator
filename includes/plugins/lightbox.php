f2a3b224653dec6d9051b7df7d301e7ae361479b
Chris Rault <Chris@chris.local>
2009-12-09 16:49:43 +0200

    colorbox.

--- includes/plugins/lightbox.php
+++ includes/plugins/lightbox.php
@@ -6,11 +6,10 @@
 		</ol>
 	</div>
 	<div id="lightbox-info" class="info-panel">
-		<h3>TopUp Lightbox</h3>
-		<p class="teaser">TopUp Lightbox is a jQuery plugin to display different types of content in a lightbox overlay.</p>
-		<p>TopUp is an easy to use Javascript library for unobtrusively displaying images and webpages in a Web 2.0 approach of popups. 
-		The library is jQuery and jQuery UI driven in order to maintain cross-browser compatibility and compactness. </p>
-		<p><a href="http://gettopup.com/" target="_blank" class="btn-link">Visit Plugin</a> <a href="#" target="_blank" class="btn-link">View Documentation</a></p>
+		<h3>Colorbox Lightbox</h3>
+		<p class="teaser">Colorbox is a light-weight, customizable lightbox plugin for jQuery, that supports photos, photo groups, slideshows, ajax, inline, and iframed content.</p>
+		<p>We'll be publishing some tutorials on how you can take advantage of this, but in the mean time you can read more on the official Colorbox website.</p>
+		<p><a href="http://colorpowered.com/colorbox/" target="_blank" class="btn-link">Visit Plugin</a> <!--<a href="#" target="_blank" class="btn-link">View Documentation</a>--></p>
 	</div>
 </div>
 

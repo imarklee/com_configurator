b780f5b47516a3f326b22f7df19252f93790bd2b
Chris Rault <Chris@Chris-Raults-iMac.local>
2010-03-25 13:49:56 +0200

    jomsocial tab

---
+++ includes/general/jomsocial.php
@@ -0,0 +1,17 @@
+<div id="jomsocial-tab" class="ui-tabs-hide ui-tabs-panel">
+	<div id="jomsocial-options" class="options-panel">
+		<h3>JomSocial Integration Settings</h3>
+		<ol class="forms">
+			<?php echo renderParams($params->renderToArray('jomsocial', 'jomsocialdata')); ?>
+		</ol>
+		<h3>JomSocial Box Styles</h3>
+		<ol class="forms">
+			<?php echo renderParams($params->renderToArray('jomsocialboxes', 'jomsocialboxesdata')); ?>
+		</ol>
+	</div>
+	<div id="jomsocial-info" class="info-panel">
+		<h3>JomSocial options overview</h3>
+		<p class="teaser"></p>
+		<p></p>
+	</div>
+</div>
--- includes/layout/manage.php
+++ includes/layout/manage.php
@@ -34,12 +34,14 @@
 					<li class="icon-debugging"><a href="#debugging-tab">Debugging</a></li>
                     <li class="icon-components"><a href="#components-tab">Component Layouts</a></li>
                     <li class="icon-comperformance"><a href="#mootools-tab">Mootools Compatibility</a></li>
+                    <li class="icon-jomsocial"><a href="#jomsocial-tab">JomSocial Integration</a></li>
 				</ul>
 				<?php include dirname(__FILE__).'/../general/general.php' ?>
 				<?php include dirname(__FILE__).'/../general/performance.php' ?>
 				<?php include dirname(__FILE__).'/../general/debugging.php' ?>
 				<?php include dirname(__FILE__).'/../general/components.php'?>
 				<?php include dirname(__FILE__).'/../general/mootools.php'?>
+				<?php include dirname(__FILE__).'/../general/jomsocial.php'?>
 			</div>
 		</div>
 

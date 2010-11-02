bc788fd44ef158807307492a1fbda938e01ab14d
Chris Rault <Chris@Chris-Raults-iMac.local>
2010-11-02 13:39:54 +0200

    Updated the Sub Header tab name to Subhead to be consistent with the block.

--- includes/blocks/subhead.php
+++ includes/blocks/subhead.php
@@ -1,13 +1,13 @@
 <?php defined('_JEXEC') or die('Restricted access'); ?>
 <div id="subhead-tab" class="ui-tabs-hide ui-tabs-panel">
 	<div id="subhead-options" class="options-panel">
-	    <h3>Sub Header Block</h3>
+	    <h3>Subheader Block</h3>
 		<ol class="forms">
 			<?php echo renderParams($params->renderToArray('subhead', 'subheaddata')); ?>
 		</ol>
 	</div>
 	<div id="subhead-info" class="info-panel">
-		<h3>Sub header block</h3>
+		<h3>Sub header</h3>
 		<p class="teaser">This block can be used compliment your sites branding, but is not limited to that.</p>
 		
 		<p>Some possible uses for this block could be an css background image, image rotator, slideshow, tabs block, intro video, etc.</p>

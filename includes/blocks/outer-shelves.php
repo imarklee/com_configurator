9276f6b7a68bd69e247293314227636a296afad6
Chris Rault <Chris@Chris-Raults-iMac.local>
2010-11-05 14:21:28 +0200

    - Updated the outer shelf block names (ie, topshelf to topshelf1) to make them consistent with the others & extended the topshelf to have 3 instances, like the bottomshelf.

--- includes/blocks/outer-shelves.php
+++ includes/blocks/outer-shelves.php
@@ -1,13 +1,21 @@
 <?php defined('_JEXEC') or die('Restricted access'); ?>
 <div id="outer-shelves-tab" class="ui-tabs-hide ui-tabs-panel">
 	<div id="shelf-options" class="options-panel">
-   	    <h3>Top Shelf Settings</h3>
+   	    <h3>Top Shelf 1 Settings</h3>
 		<ol class="forms">
-			<?php echo renderParams($params->renderToArray('topshelf', 'topshelfdata')); ?>
+			<?php echo renderParams($params->renderToArray('topshelf1', 'topshelf1data')); ?>
+		</ol>
+		<h3>Top Shelf 2 Settings</h3>
+		<ol class="forms">
+			<?php echo renderParams($params->renderToArray('topshelf2', 'topshelf2data')); ?>
+		</ol>
+		<h3>Top Shelf 3 Settings</h3>
+		<ol class="forms">
+			<?php echo renderParams($params->renderToArray('topshelf3', 'topshelf3data')); ?>
 		</ol>
    	    <h3>Bottom Shelf 1 Settings</h3>
 		<ol class="forms">
-			<?php echo renderParams($params->renderToArray('bottomshelf', 'bottomshelfdata')); ?>
+			<?php echo renderParams($params->renderToArray('bottomshelf1', 'bottomshelf1data')); ?>
 		</ol>
 		<h3>Bottom Shelf 2 Settings</h3>
 		<ol class="forms">
@@ -19,16 +27,10 @@
 		</ol>
 	</div>
 	<div id="shelves-info" class="info-panel">
-		<h3>Top &amp; bottom shelves</h3>
+		<h3>Top &amp; Bottom Shelves</h3>
 		<p class="teaser">The top &amp; bottom shelves are outside of the main content block. 
-		This means that although they are constrained to the overall sites width, they can be 
-		configured in a number of different ways.</p>
-				
-		<h4>This block is slider enabled</h4>
-		<p>This means that if enabled, the block will have a toggle link, allowing the person 
-		browsing your site to toggle the blocks visibility. The slider will use <dfn title="Cookies 
-		are small text files a web site puts on your computer to store information about your 
-		browsing that the site can access at later sessions. The following sections provide more 
-		information.">cookies</dfn> to remember the users selection.</p>		
+		This means that although the contents are constrained to the overall sites width, they can be 
+		configured to have a "wrapping div", which will span the full width of the browser window. These are useful when you need the blocks background to span the full width of the browser.</p>
+		<img src="components/com_configurator/images/outershelf-block-outlines.png" alt="outer blocks diagram" />	
 	</div>
 </div>

28dd0c18d1f58ad416d70526080a1873eaa8fa26
Andy <andrew@webmonkeys.co.za>
2011-01-20 17:33:35 +0200

    removed references to classes as they are not needed with nooku

--- depencies.php
+++ depencies.php
@@ -13,16 +13,5 @@ JLoader::register('JPath', JPATH_LIBRARIES.'/joomla/filesystem/path.php');
 JLoader::register('JArchive', JPATH_LIBRARIES.'/joomla/filesystem/archive.php');
 JLoader::register('JController', JPATH_LIBRARIES.'/joomla/application/component/controller.php');
 JLoader::register('JView', JPATH_LIBRARIES.'/joomla/application/component/view.php');
-//JLoader::register('HTML_configurator_admin', JApplicationHelper::getPath('admin_html'));
-JLoader::register('ComConfiguratorDefines', JPATH_ADMINISTRATOR.'/components/com_configurator/defines.php');
-JLoader::register('ComConfiguratorHelperUtilities', JPATH_ADMINISTRATOR.'/components/com_configurator/helpers/utilities.php');
-JLoader::register('ComConfiguratorHelperBrowser', JPATH_ADMINISTRATOR.'/components/com_configurator/helpers/browser.php');
-JLoader::register('ComConfiguratorToolbarHtml', JPATH_ADMINISTRATOR.'/components/com_configurator/toolbar.configurator.html.php');
-
-// Controllers
-JLoader::register('ComConfiguratorControllerAbstract', JPATH_ADMINISTRATOR.'/components/com_configurator/controllers/abstract.php');
-JLoader::register('ComConfiguratorControllerDispatch', JPATH_ADMINISTRATOR.'/components/com_configurator/controllers/dispatch.php');
-JLoader::register('ComConfiguratorControllerDefault', JPATH_ADMINISTRATOR.'/components/com_configurator/controllers/default.php');
-
 // Morph classes
 JLoader::register('MBrowser', JPATH_ADMINISTRATOR.'/components/com_configurator/includes/browser.php');

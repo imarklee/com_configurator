6d0c614a75caac749edf1d12a5ec36322ad5edfc
Chris <chris@prothemer.com>
2011-01-24 16:01:22 +0200

    First test commit to check that the lang strings are ok.

--- includes/blocks/desc-blocks.php
+++ includes/blocks/desc-blocks.php
@@ -2,9 +2,9 @@
 <?php if (!isset ($_COOKIE['blocks-desc'])) { ?><div class="desc-overlay">&nbsp;</div><?php } ?>
 <div id="blocks-desc" class="tab-desc"<?php if (isset ($_COOKIE['blocks-desc'])) { ?> style="display:none;"<?php } ?>>
 	<div class="desc-inner">
-		<h2>About the block settings tab</h2>
-		<p class="last">Think of blocks as the lego pieces that make up your website. Each one of the different blocks can be configured 
-		to work in a multitude of ways. These settings will change depending on which themelet is currently published.
-		<a href="#" class="close">close</a></p>
+		<h2><?= @text('About the block settings tab') ?></h2>
+		<p class="last"><?= @text('Think of blocks as the lego pieces that make up your website. Each one of the different blocks can be configured 
+		to work in a multitude of ways. These settings will change depending on which themelet is currently published.') ?>
+		<a href="#" class="close"><?= @text('close') ?></a></p>
 	</div>
 </div>

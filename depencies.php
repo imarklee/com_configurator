e2e7b29167ca8871c4cea5e802f242c6eaeee87b
Stian Didriksen <stipsan@me.com>
2011-02-25 14:55:02 +0100

    Removing no longer needed depencies

--- depencies.php
+++ depencies.php
@@ -11,7 +11,4 @@ JLoader::register('JFile', JPATH_LIBRARIES.'/joomla/filesystem/file.php');
 JLoader::register('JFolder', JPATH_LIBRARIES.'/joomla/filesystem/folder.php');
 JLoader::register('JPath', JPATH_LIBRARIES.'/joomla/filesystem/path.php');
 JLoader::register('JArchive', JPATH_LIBRARIES.'/joomla/filesystem/archive.php');
-JLoader::register('JController', JPATH_LIBRARIES.'/joomla/application/component/controller.php');
-JLoader::register('JView', JPATH_LIBRARIES.'/joomla/application/component/view.php');
-// Morph classes
 JLoader::register('MBrowser', JPATH_ADMINISTRATOR.'/components/com_configurator/includes/browser.php');

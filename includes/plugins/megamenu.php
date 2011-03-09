a6a6167c4abe05f4aff0e63992f841c3a2385810
Stian Didriksen <stipsan@me.com>
2011-03-09 15:57:09 +0100

    Nookuifying megamenu layout

--- includes/plugins/megamenu.php
+++ includes/plugins/megamenu.php
@@ -1,13 +1,13 @@
 <?php defined('_JEXEC') or die('Restricted access'); ?>
 <div id="megamenu-tab" class="ui-tabs-hide">
 	<div id="megamenu-options" class="options-panel">
-	    <h3>Mega Shelf Menu Settings</h3>
+	    <h3><?= @text('Mega Shelf Menu Settings') ?></h3>
 		<ol class="forms">
-			<?php echo renderParams($params->renderToArray('megamenu', 'megamenudata')); ?>
+			<?= @params(array('name' => 'megamenu')) ?>
 		</ol>
 	</div>
 	<div id="megamenu-info" class="info-panel">
-		<h3>Mega Shelf Menu</h3>
+		<h3><?= @text('Mega Shelf Menu') ?></h3>
 		<p class="teaser"></p>
 	</div>
 </div>

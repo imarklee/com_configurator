60e4d7d00e5652c40f34360bc56145319cec86f0
Chris <chris@prothemer.com>
2011-03-11 18:26:15 +0200

    commented out the module chrome options for now.

--- includes/general/enhancements.php
+++ includes/general/enhancements.php
@@ -5,10 +5,10 @@
 		<ol class="forms">
 			<?php echo renderParams($params->renderToArray('articleenhancements', 'articleenhancementsdata')); ?>
 		</ol>
-		<h3>Article module position chromes</h3>
+		<!--<h3>Article module position chromes</h3>
 		<ol class="forms">
 			<?php echo renderParams($params->renderToArray('articlepositions', 'articlepositionsdata')); ?>
-		</ol>
+		</ol>-->
 		<h3>Blog view enhancements</h3>
 		<ol class="forms">
 			<?php echo renderParams($params->renderToArray('blogenhancements', 'blogenhancementsdata')); ?>
@@ -18,7 +18,7 @@
 		<h3>Core Enhancements overview</h3>
 		<p class="tease">The Core Enhancements extend Joomla's core functionality via the templates html overrides.</p>
 		<p>These options let you decided which of these enhancements are enabled.</p>
-		<h4>Article module positions</h4>
-		<p>There are 6 module positions that you can use to display modules within your content. The module position names are <strong>article_top1-3</strong> &amp; <strong>article_bottom1-3</strong>. To use, simply publish your module to any of the positions &amp; set the module chrome for that position.</p>
+		<!--<h4>Article module positions</h4>
+		<p>There are 6 module positions that you can use to display modules within your content. The module position names are <strong>article_top1-3</strong> &amp; <strong>article_bottom1-3</strong>. To use, simply publish your module to any of the positions &amp; set the module chrome for that position.</p>-->
 	</div>
 </div>

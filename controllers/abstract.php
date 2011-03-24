7805cf13413150bc27f058c93d4bd9c649eba984
Stian Didriksen <stipsan@me.com>
2011-03-24 17:02:44 +0100

    moving uni_installer action to installer controller (for now)

--- controllers/abstract.php
+++ controllers/abstract.php
@@ -276,57 +276,6 @@ class ComConfiguratorControllerAbstract extends ComDefaultControllerDefault
 		$this->_redirect = 'index.php?option=com_configurator&view='.$this->_identifier->name;
 		return $rowset;
 	}
-
-	protected function _actionUni_installer()
-	{
-		$mem_limit = ini_get('memory_limit');
-		if(str_replace('M', '', $mem_limit) < 128){ ini_set('memory_limit', '128M'); }
-		
-			$return = array();		
-			if( JRequest::getVar('do') && JRequest::getVar('do') == 'upload' ){
-				$install_type =  JRequest::getVar('itype');
-				$the_files = JRequest::getVar( 'insfile', null, 'files', 'array' );
-				if($install_type != 'undefined'){
-					if($the_files['name'] != ''){
-						switch($install_type){
-							case 'themelet':
-							$return = $this->themelet_upload();
-							break;
-							case 'template':
-							$return = $this->template_upload();
-							break;
-							case 'logo':
-							$return = $this->logo_upload();
-							break;
-							case 'background':
-							$return = $this->background_upload();
-							break;
-							case 'favicon':
-							$return = $this->favicon_upload();
-							break;
-							case 'iphone':
-							$return = $this->iphone_upload();
-							break;
-							case 'sample':
-							$return = $this->demo_upload();
-							break;
-							case 'themelet_assets':
-							$return = $this->assets_upload();
-							break;
-						}
-					}else{
-						$return['error'] = "No file has been selected. Please select a file in <strong>Step 2</strong> and try again.";
-					}
-				}else{
-					$return['error'] = "No install type has been selected. Please make a selection in <strong>Step 1</strong> and try again.";
-				}
-			}else{
-				$return['error'] = "Upload failed: No post data!";
-			}
-			
-			echo json_encode($return);
-			die();
-	}
 	
 	function assets_upload(){
 		$assetsdir 	= JPATH_SITE.'/morph_assets';

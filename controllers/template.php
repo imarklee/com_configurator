4f637ea1b5e7cf37c2e6597c8d4e638be26d5b6b
Stian Didriksen <stipsan@me.com>
2011-03-24 16:47:50 +0100

    adding template controller

---
+++ controllers/template.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @package   Configurator Component
+* @author    Prothemer http://www.prothemer.com
+* @copyright Copyright (C) 2008 - 2010 Web Monkeys Design Studio CC.
+* @license   http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @desc      Originally based on Tatami from Ninja Forge. http://www.ninjaforge.com
+*/
+
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * ComConfiguratorControllerTemplate
+ *
+ * For updating the Morph base template.
+ * 
+ * @author Stian Didriksen <stian@prothemer.com>
+ */
+ class ComConfiguratorControllerTemplate extends ComConfiguratorControllerDefault
+ {
+ 	// Do nothing here yet
+ }

69fd282901b9cb449ab81a2d3e5bd8efbe6cc253
Stian Didriksen <stipsan@me.com>
2011-03-24 16:33:36 +0100

    adding new controllers to simplify code

---
+++ controllers/installer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @package   Configurator Component
+* @author    Prothemer http://www.prothemer.com
+* @copyright Copyright (C) 2008 - 2010 Web Monkeys Design Studio CC.
+* @license   http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @desc      Originally based on Tatami from Ninja Forge. http://www.ninjaforge.com
+*/
+
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * ComConfiguratorControllerInstaller
+ *
+ * Installs Configurator, and only Configurator. Themelet, Morph and assets are done in their own controllers
+ * 
+ * @author Stian Didriksen <stian@prothemer.com>
+ */
+ class ComConfiguratorControllerInstaller extends ComConfiguratorControllerDefault
+ {
+ 	// Do nothing here yet
+ }
---
+++ controllers/themelet.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @package   Configurator Component
+* @author    Prothemer http://www.prothemer.com
+* @copyright Copyright (C) 2008 - 2010 Web Monkeys Design Studio CC.
+* @license   http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @desc      Originally based on Tatami from Ninja Forge. http://www.ninjaforge.com
+*/
+
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * ComConfiguratorControllerThemelet
+ *
+ * The themelet configurator controller, for installing and such
+ * 
+ * @author Stian Didriksen <stian@prothemer.com>
+ */
+ class ComConfiguratorControllerThemelet extends ComConfiguratorControllerDefault
+ {
+ 	// Do nothing here yet
+ }

d37577bcedb34a7dff1abbdeb609e33f9cfb6992
Chris Rault <chris@prothemer.com>
2011-08-31 15:01:27 +0200

    Tweaked the installers layout & install types text.

--- includes/tools/uploader.php
+++ includes/tools/uploader.php
@@ -4,14 +4,16 @@
 	    <h2>Universal Installer</h2>		
 		<div class="upload-tool">
 			<fieldset id="install-type">
+				<h3>Install</h3>
 				<label for="upload_template"> <input type="radio" name="upload_type" value="template" id="upload_template" />Upgrade Morph</label>
 				<label for="upload_themelet"> <input type="radio" name="upload_type" value="themelet" id="upload_themelet" />Install / Upgrade Themelet</label>
 				<label for="upload_themelet_assets"><input type="radio" name="upload_type" value="themelet_assets" id="upload_themelet_assets" />Upload Themelet Assets</label>
 				<label for="upload_sample"><input type="radio" name="upload_type" value="sample" id="upload_sample" />Install Demo Content</label>
-				<label for="upload_logo"><input type="radio" name="upload_type" value="logo" id="upload_logo" />Upload Logo</label>
-				<label for="upload_background"><input type="radio" name="upload_type" value="background" id="upload_background" />Upload Background</label>
-				<label for="upload_iphone"><input type="radio" name="upload_type" value="iphone" id="upload_iphone" />Upload iPhone</label>
-				<label for="upload_favicon"><input type="radio" name="upload_type" value="favicon" id="upload_favicon" />Upload Favicon</label>
+				<h3>Upload</h3>
+				<label for="upload_logo"><input type="radio" name="upload_type" value="logo" id="upload_logo" />Your Site's Logo</label>
+				<label for="upload_background"><input type="radio" name="upload_type" value="background" id="upload_background" />A Background Image</label>
+				<label for="upload_iphone"><input type="radio" name="upload_type" value="iphone" id="upload_iphone" />iPhone Header/Webclip</label>
+				<label for="upload_favicon"><input type="radio" name="upload_type" value="favicon" id="upload_favicon" />A Favicon</label>
 			</fieldset>
 			<fieldset id="select-file">
 			    <h4><strong>Step 1:</strong> Choose an install type</h4>
